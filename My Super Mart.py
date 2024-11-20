6d378b806c0cadd33c7f56875ee6388ee87b36af
Muhammad Saad Nomani <107791651+saadnom@users.noreply.github.com>
2024-11-21 02:00:56 +0500

    Update My Super Mart.py

--- My Super Mart.py
+++ My Super Mart.py
@@ -1,22 +1,23 @@
 items = {"Laptop":100000, 'Mobile':50000, 'Printer':30000, 'Head Phone':15000, 'Speaker':5000}
 print("Hey! Welcome To My Super Mart")
-order = input("Enter an item: ")
 discount = 20
 total_price = 0
-if order in items:
-    total_price += items[order]
-    print(f"You ordered: {order}")
-user = input("Do you want to buy further more items? ")
-if user == "Yes":
-    order_2 = input("Enter an item: ")
-    if order_2 in items:
-        total_price += items[order_2]
-        print(f"You ordered: {order_2}")
+while True:
+    order = input("Enter an item: ")
+    if order in items:
+        total_price += items[order]
+        print(f"You ordered: {order} Price: {items[order]}")
     else:
         print(f"{order} is not available yet.")
+
+    user = input("Do you want to buy further more items? ")
+    if user == "NO":
+        break
+
 if total_price >= 100000:
-    print(f"Your total price is: {total_price}")
-    print(f"Discounted Price: {total_price * (1 - discount/100)}")
+    print(f"Your total bill is: {total_price}")
+    print(f"After Discount Price: {total_price * (1 - discount/100)}")
+    print(f"Thank You For Shopping!")
 elif total_price < 100000:
-    print(f"Your total price is: {total_price}")
+    print(f"Your total price is: {total_price} ")
     print(f"Your total price is not eligible for discount. Thank you!")
